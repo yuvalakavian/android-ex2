66e772e8c551be2f74f97cf68d897a26f37ab2c0
Yuval Akavian <yuvalmetal@gmail.com>
2025-01-14 23:25:27 +0200

    changed ids

--- app/src/main/java/com/example/ex2/AddStudentActivity.kt
+++ app/src/main/java/com/example/ex2/AddStudentActivity.kt
@@ -16,18 +16,18 @@ class AddStudentActivity : AppCompatActivity() {
 
         val button_add_student = findViewById<Button>(R.id.button_add_student)
         val button_cancel = findViewById<Button>(R.id.button_cancel)
-        val edit_text_id = findViewById<EditText>(R.id.editText_id)
-        val edit_text_name = findViewById<EditText>(R.id.editText_name)
-        val edit_text_address = findViewById<EditText>(R.id.editText_address)
-        val edit_text_phone = findViewById<EditText>(R.id.editText_phone)
-        val checkbox_checked = findViewById<CheckBox>(R.id.checkbox_checked)
+        val activity_add_student_edittext_id = findViewById<EditText>(R.id.activity_add_student_edittext_id)
+        val activity_add_student_edittext_name = findViewById<EditText>(R.id.activity_add_student_edittext_name)
+        val activity_add_student_edittext_address = findViewById<EditText>(R.id.activity_add_student_edittext_address)
+        val activity_add_student_edittext_phone = findViewById<EditText>(R.id.activity_add_student_edittext_phone)
+        val activity_add_student_checkbox_checked = findViewById<CheckBox>(R.id.activity_add_student_checkbox_checked)
 
         button_add_student.setOnClickListener {
-            val id = edit_text_id.text.toString().trim()
-            val name = edit_text_name.text.toString().trim()
-            val address = edit_text_address.text.toString().trim()
-            val phone = edit_text_phone.text.toString().trim()
-            val checkbox_checked_val = checkbox_checked.isChecked()
+            val id = activity_add_student_edittext_id.text.toString().trim()
+            val name = activity_add_student_edittext_name.text.toString().trim()
+            val address = activity_add_student_edittext_address.text.toString().trim()
+            val phone = activity_add_student_edittext_phone.text.toString().trim()
+            val checkbox_checked_val = activity_add_student_checkbox_checked.isChecked
 
             if (id.isEmpty() || name.isEmpty() || address.isEmpty() || phone.isEmpty()) {
                 Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
